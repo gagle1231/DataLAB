5949fb6f85f1a644f8984132989d1b64cd179000
gagle1231 <kke1231@naver.com>
2024-09-22 02:50:51 +0900

    feat: add error log

--- backend/src/main/java/com/onion/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/onion/backend/exception/GlobalExceptionHandler.java
@@ -1,17 +1,19 @@
 package com.onion.backend.exception;
 
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(BaseException ex) {
         ErrorResponse errorResponse = ErrorResponse.create(ex, ex.getStatus(), ex.getMessage());
+        log.error(ex.getStatus()+":: "+ex.getMessage());
         return new ResponseEntity<>(errorResponse, ex.getStatus());
     }
 }
